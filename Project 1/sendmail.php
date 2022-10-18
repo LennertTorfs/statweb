713a1e6368d26beef27219086d8f18ce6898a216
Lennert Torfs <r0791450@student.thomasmore.be>
2022-10-19 00:41:44 +0200

    contact

---
+++ Project 1/sendmail.php
@@ -0,0 +1,10 @@
+<?php
+$to = "r0791450@student.thomasmore.be";
+$subject = "onderwerp";
+$message = "Hello world!";
+$headers = "From: yourname@yourdomainname.be"
+mail($to, $subject, $message, $headers);
+$whatever = $_POST["naam"]
+$whatever = $_POST["onderwerp"]
+
+?>
