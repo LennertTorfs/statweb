a2d4876528917e8b809e05e93faa2319bdde48bc
Lennert Torfs <r0791450@student.thomasmore.be>
2022-11-13 22:25:27 +0100

    Added Contact, about me, hobby, cv, eraring

--- Projecten/Project 1/sendmail.php
+++ Projecten/Project 1/sendmail.php
@@ -1,8 +1,8 @@
 <?php
-$to = "r0791450@student.thomasmore.be";
+$to = "contact@lennerttorfs.be";
 $subject = "onderwerp";
 $message = "Hello world!";
-$headers = "From: yourname@yourdomainname.be"
+$headers = "From: lennerttorfs@hotmail.be"
 mail($to, $subject, $message, $headers);
 $whatever = $_POST["naam"]
 $whatever = $_POST["onderwerp"]
