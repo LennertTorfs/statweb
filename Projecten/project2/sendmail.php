1fdb265490ceac71aed7a070f54d768a12e0c1fc
Lennert Torfs <r0791450@student.thomasmore.be>
2022-12-13 15:49:37 +0100

    Added project 2

---
+++ Projecten/project2/sendmail.php
@@ -0,0 +1,10 @@
+<?php
+$to = "contact@lennerttorfs.be";
+$subject = "onderwerp";
+$message = "Hello world!";
+$headers = "From: lennerttorfs@hotmail.be"
+mail($to, $subject, $message, $headers);
+$whatever = $_POST["naam"]
+$whatever = $_POST["onderwerp"]
+
+?>
