f03c554d495dbb9a3e7bab5db58fad2ea36ca15e
Lennert <r0791450@student.thomasmore.be>
2023-04-09 23:27:52 +0200

    regestratie

--- Projecten/project2/registratie.js
+++ Projecten/project2/registratie.js
@@ -40,11 +40,12 @@ const betaling = document.getElementById("betaling");
 //https://www.youtube.com/watch?v=msz6g62PimM&ab_channel=LearnCoding
 //https://www.youtube.com/watch?v=35SxoozB6vQ&ab_channel=M-softTech
 //https://stackoverflow.com/questions/13717648/how-to-display-multiple-error-messages-on-the-same-page-using-javascript
-document.getElementById("submit-button").addEventListener("click",valideteForm());
+const knop = document.getElementById('verzenden')
+knop.addEventListener("click",validateForm);
 
 let errors = [];
 
-function valideteForm(){
+function validateForm(){
     
     let naam = document.getElementById('naam').value;
     let voornaam = document.getElementById('voornaam').value;
@@ -56,15 +57,42 @@ function valideteForm(){
     let land = document.getElementById('land').value;
     let provincie = document.getElementById('provincie').value;
     let postcode = document.getElementById('postcode').value;
-    let veld = document.form.wijze;
 
-    naam.checkEmptyField
+    checkEmptyField(naam, 'Het veld naam is vereist.');
+    checkEmptyField(voornaam, 'Het veld voornaam is vereist.');
+    checkEmptyField(gebruikersnaam, 'Het veld gebruikersnaam is vereist.');
+    checkEmptyField(email, 'het veld email is vereist.');
+    checkEmptyField(wachtwoord, 'Het veld wachtwoord is vereist.');
+    checkEmptyField(wachtwoord2, 'Het veld herhaal wachtwoord is vereist.');
+    checkEmptyField(land, 'Land is vereist.');
+    checkEmptyField(adres, 'Adres is vereist.');
+    checkEmptyField(provincie, 'Provincie is vereist.');
+    checkEmptyField(postcode, 'Het veld postcode is vereist.');
+
+    validateEmail(email);
+    if (!validateEmail(email)){
+      errors.push('E-mailaders is niet correct.')
+    }
+
+    if (wachtwoord.length < 8){
+      errors.push('Je wachtwoord moet minstens 8 tekens lang zijn');
+    }
+    else if (wachtwoord != wachtwoord2){
+      error.push('Je wachtwoorden komen niet overeen')
+    }
+
+    if (!getElementById('voorwaarden').checked){
+      errors.push('Je moet de algemene voorwaarden accepteren.')
+    }
 
 
-    if (errors != ''){
-      let errorBox = document.getElementById(errorV) 
 
-      errorBox.classList.remove('visually-hidden');
+    if (errors.length > 0){
+      const errorElements = document.querySelectorAll('.error');
+      errorElements.forEach(element => {
+        element.textContent = errors.join('<br>');
+        element.classList.remove('visually-hidden');
+      });
     }
     else{
       let goed = document.getElementById(succes);
@@ -91,8 +119,7 @@ function checkEmptyField(veld, melding){
 
 function validatePayment(veld){
   
-  let bool = false
-  
+  let bool = false;
 
   for (let i=0;i=veld;i++){
     if (x[i].checked){
@@ -105,10 +132,10 @@ function validatePayment(veld){
 
   if (!bool){
     let bericht = 'je betalingswijze is ' + veld;
-    document.getElementById('methode').innerText = bericht;
+    //document.getElementById('methode').innerText = bericht;
+    document.replaceChild(bericht, 'Aww')
     return true;
   }
-  
 }
 
 function checkPC(veld){
@@ -121,8 +148,7 @@ function checkPC(veld){
       return true;
     }
     else{
-      error.push('De waarde van postcode moet tussen 1000 en 9999 liggen');
+      errors.push('De waarde van postcode moet tussen 1000 en 9999 liggen');
     }
   }
-  
 }
