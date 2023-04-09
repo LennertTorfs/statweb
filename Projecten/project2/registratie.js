5edec2e8f2878f4541ed8649eb786af38deb1586
Lennert <r0791450@student.thomasmore.be>
2023-04-09 17:52:30 +0200

    adjustments

--- Projecten/project2/registratie.js
+++ Projecten/project2/registratie.js
@@ -40,21 +40,89 @@ const betaling = document.getElementById("betaling");
 //https://www.youtube.com/watch?v=msz6g62PimM&ab_channel=LearnCoding
 //https://www.youtube.com/watch?v=35SxoozB6vQ&ab_channel=M-softTech
 //https://stackoverflow.com/questions/13717648/how-to-display-multiple-error-messages-on-the-same-page-using-javascript
-document.getElementById("submit-button").addEventListener("click",valideteForm);
+document.getElementById("submit-button").addEventListener("click",valideteForm());
 
-let errors[];
+let errors = [];
 
 function valideteForm(){
-    let naam = document.getElementById('naam');
-    let voornaam = document.getElementById('voornaam');
-    let gebruikersnaam = document.getElementById('gebruikersnaam');
-    let email = document.getElementById('inputEmail4');
-    let wachtwoord = document.getElementById('inputPassword4');
-    let wachtwoord2 = document.getElementById('inputPassword42');
-    let adres = document.getElementById('adres');
-    let land = document.getElementById('land');
-    let provincie = document.getElementById('provincie');
-    let postcode = document.getElementById('postcode');
+    
+    let naam = document.getElementById('naam').value;
+    let voornaam = document.getElementById('voornaam').value;
+    let gebruikersnaam = document.getElementById('gebruikersnaam').value;
+    let email = document.getElementById('inputEmail4').value;
+    let wachtwoord = document.getElementById('inputPassword4').value;
+    let wachtwoord2 = document.getElementById('inputPassword42').value;
+    let adres = document.getElementById('adres').value;
+    let land = document.getElementById('land').value;
+    let provincie = document.getElementById('provincie').value;
+    let postcode = document.getElementById('postcode').value;
+    let veld = document.form.wijze;
+
+    naam.checkEmptyField
 
 
+    if (errors != ''){
+      let errorBox = document.getElementById(errorV) 
+
+      errorBox.classList.remove('visually-hidden');
+    }
+    else{
+      let goed = document.getElementById(succes);
+      document.getElementById('betalingswijze').classList.remove('visually-hidden');
+
+      goed.classList.remove('visually-hidden');
+
+    }
+
+}
+
+function checkEmptyField(veld, melding){
+  if (veld == '' || veld == null){
+    errors.push(melding)
+    return 0;
+  }
+
+  //https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
+  function validateEmail(emailadres){
+    let re = /\S+@\S+\.\S+/;
+    return re.test(emailadres);
+  }
+}
+
+function validatePayment(veld){
+  
+  let bool = false
+  
+
+  for (let i=0;i=veld;i++){
+    if (x[i].checked){
+      bool = true;
+    }
+    else{
+      bool = false;
+    }
+  }
+
+  if (!bool){
+    let bericht = 'je betalingswijze is ' + veld;
+    document.getElementById('methode').innerText = bericht;
+    return true;
+  }
+  
+}
+
+function checkPC(veld){
+  if (veld == '' || veld == null){
+    errors.push('Het veld postcode is vereist');
+    return 0;
+  }
+  else{
+    if (veld >= 1000 && veld < 10000){
+      return true;
+    }
+    else{
+      error.push('De waarde van postcode moet tussen 1000 en 9999 liggen');
+    }
+  }
+  
 }
